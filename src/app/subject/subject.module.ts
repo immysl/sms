f2c9cd4ff3fefb319c8bc24a7e5d2b7e28d00e43
Ahamed Imran <immy123@gmail.com>
2017-04-08 23:49:55 +0530

    added subject component

---
+++ src/app/subject/subject.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubjectComponent } from './subject.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SubjectComponent]
+})
+export class SubjectModule { }
