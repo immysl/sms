56e84401006a24fbdd32d4c71370c5b11b24963d
Ahamed Imran <immy123@gmail.com>
2017-04-15 14:23:53 +0530

    made login component redirect back if already login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../auth.service';
@@ -11,10 +12,16 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private router: Router,
+    private location: Location,
     private authService: AuthService
   ) { }
 
   ngOnInit() {
+    this.authService.checkLogin().subscribe(isLoggedIn => {
+      if (isLoggedIn) {
+        this.location.back();
+      }
+    });
   }
 
   login() {
